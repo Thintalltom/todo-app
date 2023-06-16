aa1d5d8ee4a6699c5554328aec19ad000fcf03c9
tomide <tomideadeyanju@gmail.com>
2023-06-16 18:08:32 +0100

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Form from "../Component/Form";
 import "./App.css";
 import TodoList from "../Component/TodoList";
 import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
-const localInfo = JSON.parse(localStorage.getItem('todos'))
+const localInfo = JSON.parse(localStorage.getItem('todos') || "[]")
 
 function App() {
   const [Info, setInfo] = useState("");
